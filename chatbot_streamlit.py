e6baab6f036696154ac0c2598366b83044dbc6c5
KevinH <khui98@gmail.com>
2025-04-05 19:23:17 -0400

    Uploaded 2 chatbot python apps
    Uploaded 2 chatbot python apps using ChatOllama, langchain, and streamlit

---
+++ chatbot_streamlit.py
@@ -0,0 +1,56 @@
+# AI Chatbot using ollama
+from langchain_ollama import ChatOllama
+from langchain.schema import SystemMessage, HumanMessage, AIMessage
+import streamlit as st
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+MODEL = "llama3.2"
+
+def main():
+    st.set_page_config(page_title="My Chatbot", page_icon="🤖")
+    st.header("My Chatbot 🤖")
+
+    # Aligns chatbot text and icon to right of screen
+    st.markdown("""
+        <style>
+            .st-emotion-cache-4oy321 {
+                flex-direction: row-reverse;
+                text-align: right;
+            }
+        </style>
+    """,
+    unsafe_allow_html=True)
+
+    # Checks if previous "messages" are in session_state, otherwise "messages" are remembered for the current conversation
+    if "messages" not in st.session_state:    
+        st.session_state.messages = [SystemMessage(content="Hello! How can I help you today?")]
+
+    # Ollama llm setup
+    llm = ChatOllama(model=MODEL, temperature=0)
+
+    chatbot_message = st.chat_message("assistant")
+    chatbot_message.write("Hello user!")
+    
+    # Take user input
+    user_input = st.chat_input("Talk to the chatbot")
+    if user_input:
+        user_message = st.chat_message("human")
+        user_message.write(user_input)
+        logging.info("Message sent...")
+        chatbot_message = st.chat_message("assistant")
+
+        # Sends user input to llm model and returns content
+        st.session_state.messages.append(HumanMessage(content=user_input))
+        logging.info("Processing message...")
+        response = llm.invoke(st.session_state.messages)
+
+        # Appends all messages to "messages" session_state
+        st.session_state.messages.append(AIMessage(content=response.content))
+        chatbot_message.write(response.content)
+        logging.info("Response sent...")
+
+if __name__ == '__main__':
+    main()
---
+++ chatbot_streamlit_chat.py
@@ -0,0 +1,61 @@
+# AI Chatbot using ollama that shows chat history with streamlit_chat module
+from langchain_ollama import ChatOllama
+from langchain.schema import SystemMessage, HumanMessage, AIMessage
+import streamlit as st
+from streamlit_chat import message
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+MODEL = "llama3.2"
+
+def main():
+    st.set_page_config(page_title="My Chatbot", page_icon="🤖")
+    st.header("My Chatbot 🤖")
+
+    # Aligns user text and icon to right of screen
+    st.markdown("""
+        <style>
+            .st-emotion-cache-4oy321 {
+                flex-direction: row-reverse;
+                text-align: right;
+            }
+        </style>
+    """,
+    unsafe_allow_html=True)
+
+    # Checks if previous "messages" are in session_state, otherwise "messages" are remembered for the current conversation
+    if "messages" not in st.session_state:    
+        st.session_state.messages = [SystemMessage(content="Hello! How can I help you today?")]
+
+    # Ollama llm setup
+    llm = ChatOllama(model=MODEL, temperature=0)
+    
+    # Take user input
+    user_input = st.chat_input("Talk to the chatbot")
+    if user_input:
+        logging.info("Message sent...")
+
+        # Sends user input to llm model and returns content
+        st.session_state.messages.append(HumanMessage(content=user_input))
+        logging.info("Processing message...")
+        response = llm.invoke(st.session_state.messages)
+
+        # Appends all messages to "messages" session_state
+        st.session_state.messages.append(AIMessage(content=response.content))
+        logging.info("Response sent...")
+
+    # Sets "messages" in session_state to empty list
+    messages = st.session_state.get("messages", [])
+
+    for i, msg in enumerate(messages):
+        if i % 2 == 0:
+            message(msg.content, is_user=False, key=str(i) + "_ai")
+        else:
+            message(msg.content, is_user=True, key=str(i) + "_user")
+
+
+
+if __name__ == '__main__':
+    main()
